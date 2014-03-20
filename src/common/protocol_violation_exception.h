c8eee73f1993e9738f5db9fd1ed3cb4452f09c9c
Lukas <Lukas>
2014-03-20 16:39:50 +0100

    protocol_violation_exception modified

--- src/common/protocol_violation_exception.h
+++ src/common/protocol_violation_exception.h
@@ -1,7 +1,18 @@
+#include <string>
 
 #ifndef PROTOCOL_VIOLATION_EXCEPTION_H
 #define PROTOCOL_VIOLATION_EXCEPTION_H
 
 
-struct ProtocolViolationException {};
+struct ProtocolViolationException {
+	std::string msg;
+	std::string err;
+	int protMsg;
+	int codeNbr;
+	ProtocolViolationException(std::string m, int prot, int code):
+		msg(m), protMsg(prot), codeNbr(code) {}
+
+	ProtocolViolationException(std::string m, std::string em):
+		msg(m), err(em) {}
+};
 #endif
