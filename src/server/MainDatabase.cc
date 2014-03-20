1c572212889ec9a09649a6c3fbc106bccbef86fe
Christian Frid <ada10cfr@hacke-6.student.lth.se>
2014-03-20 12:06:19 +0100

    fyllt i en metod i maindatabase

--- src/server/MainDatabase.cc
+++ src/server/MainDatabase.cc
@@ -6,13 +6,20 @@ using namespace std;
 class MainDatabase : public Database {
 public:
 
-	MainDatabase(){
+	MainDatabase(){}
 
-	
-	}
-
-	virtual string& listNewsGroup(const string& ngName){
+	virtual string& listNewsGroups() const{
+		map<string& newsGroup, map<string& atricleName, string& theText>>::iterator it;
 		string s("");
+		for(it = database.begin(); it != database.end(); ++it){
+			s.append(it->first);
+			s.append(endl);
+		}
+		if(s == ""){
+			s.apppend("No news groups in database.");
+			s.apppend(endl);
+			return s;
+		}
 		return s;
 	}
 
@@ -47,6 +54,6 @@ public:
 	}
 
 private:
-
+	map<string& newsGroup, map<string& atricleName, string& theText>> database;
 };
 
